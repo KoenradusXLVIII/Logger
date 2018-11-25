888baabe5174f8562d28d96173e0f6ecddf75c21
LAP-EHV-JV\Joost <joost.verberk@gmail.com>
2018-11-25 19:58:50 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='logger',
+      version='0.1',
+      description='Wrapper for the logging module',
+      url='https://github.com/KoenradusXLVIII/SmartHouse/tree/master/python/logger',
+      author='Joost Verberk',
+      packages=['logger'],
+      zip_safe=False)
